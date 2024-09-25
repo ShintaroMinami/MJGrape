e1f92354d0a4d2c4e0b91dda7cd005f4361dd7de
ShintaroMinami <sminami@neoleukin.com>
2024-09-25 00:16:55 -0700

    added io.py

--- mj_grape/io.py
+++ mj_grape/io.py
@@ -7,16 +7,6 @@ import copy
 
 MAX_PAI_NUMBER = 136
 
-mjlog_file = 'hoge.mjlog'
-import sys
-mjlog_file = sys.argv[1]
-
-mjdata_dict = parse_mjlog(load_mjlog(mjlog_file))
-meta_data = mjdata_dict['meta']
-
-player_rate = [float(player['rate']) for player in meta_data['UN']]
-rule_set = meta_data['GO']['config']
-
 class RoundSimulator():
     def __init__(self, player_rate=None):
         self.player_rate = player_rate
@@ -28,6 +18,7 @@ class RoundSimulator():
         self.player_last_discard = -1
         self.player_reach_step = [0, 0, 0, 0]
         self.player_online = [True, True, True, True]
+        self.tile_last_discarded = -1
         return
     
     def parse_init(self, data):
@@ -35,6 +26,7 @@ class RoundSimulator():
         self.scores = data['scores']
         self.hands = data['hands']
         self.melds = [[], [], [], []]
+        self.melds_indicator = [[], [], [], []]
         self.info = {
             'round': int(data['round']),
             'combo' : int(data['combo']),
@@ -43,19 +35,21 @@ class RoundSimulator():
         self.dora_indicator.append(int(data['dora']))
         self.pai_open[self.dora_indicator] = True
 
-    def update_datalist(self, player, discard_pai, is_tsumo_giri):
+    def update_datalist(self, player, discard_pai, is_tsumo_giri, is_skip=False):
         kyokumen = {
             'player': player,
             'discard': discard_pai,
             'is_tsumo_giri': is_tsumo_giri,
-            'riich_steps': list(self.player_reach_step),
-            'online': list(self.player_online),
-            'oya': list(self.leader),
+            'is_skip': is_skip,
+            'riich_state': self.player_reach_step[player],
+            'hands': copy.deepcopy(self.hands[player]),
+            'melds': copy.deepcopy(self.melds[player]),
+            'melds_indicator': copy.deepcopy(self.melds_indicator[player]),
+            'online': self.player_online[player],
+            'oya': self.leader[player],
             'dora_indicator': list(self.dora_indicator),
-            'scores': list(self.scores),
+            'score': self.scores[player],
             'info': copy.deepcopy(self.info),
-            'hands': copy.deepcopy(self.hands),
-            'melds': copy.deepcopy(self.melds),
         }
         self.kyokumen_list.append(kyokumen)
 
@@ -64,12 +58,15 @@ class RoundSimulator():
             player = int(data['player'])
             tile = int(data['tile'])
             num_skip = 0 if self.player_last_discard == -1 else (player - self.player_last_discard) % 4 - 1 
-            for _ in range(num_skip):
-                self.update_datalist(-1, MAX_PAI_NUMBER, False)
+            for i in range(num_skip):
+                ip = player - num_skip + i
+                ip = 3 if ip == -1 else ip
+                self.update_datalist(ip, MAX_PAI_NUMBER, False, is_skip=True)
             is_tsumo_giri = (self.player_last_draw == player) and (self.hands[player][-1] == tile)
             self.hands[player] = [i for i in self.hands[player] if i != tile]
             self.update_datalist(player, tile, is_tsumo_giri)
             self.player_last_discard = player
+            self.tile_last_discarded = tile
         if tag == 'DRAW':
             player = int(data['player'])
             tile = int(data['tile'])
@@ -82,9 +79,21 @@ class RoundSimulator():
             call_type = data['call_type']
             if call_type == 'AnKan':
                 mentsu = list(range(mentsu[0], mentsu[0]+4))
-            if call_type == 'KaKan':
-                self.melds[caller] = [m for m in self.melds[caller] if not set(m) <= set(mentsu)]
+                melds_indicator = [0, 0, 0, 0]
+            elif call_type == 'KaKan':
+                non_Kan = [(m,z) for (m,z) in zip(self.melds[caller],self.melds_indicator[caller]) if not set(m) <= set(mentsu)]
+                old_mentsu, old_indicator = [(m,z) for (m,z) in zip(self.melds[caller],self.melds_indicator[caller]) if set(m) <= set(mentsu)][0]
+                old_gotten = int(np.array(old_mentsu)[np.array(old_indicator, dtype=bool)][0])
+                new_gotten = list((set(mentsu) - set(old_mentsu)))[0]
+                melds_indicator = ((np.array(mentsu) == old_gotten)*1 + (np.array(mentsu) == new_gotten)*2).astype(int).tolist()
+                if len(non_Kan) == 0:
+                    self.melds[caller], self.melds_indicator[caller] = [], []
+                else:
+                    self.melds[caller], self.melds_indicator[caller] = [list(a) for a in zip(*non_Kan)]
+            else:
+                melds_indicator = (np.array(mentsu) == self.tile_last_discarded).astype(int).tolist()
             self.melds[caller].append(mentsu)
+            self.melds_indicator[caller].append(melds_indicator)
             self.hands[caller] = [i for i in self.hands[caller] if not i in mentsu]
         if tag == 'REACH':
             player = int(data['player'])
@@ -111,14 +120,25 @@ class RoundSimulator():
         else:
             self.parse_action(tag, data)
 
-
-for round in mjdata_dict['rounds']:
-    roundsim = RoundSimulator(player_rate=player_rate)
-    for data in round:
-        roundsim.add(data)
-    kyokumen_list = copy.deepcopy(roundsim.kyokumen_list)
-    print(kyokumen_list)
-    print(roundsim.result)
-    exit()
+    def read_round(self, round):
+        for data in round:
+            self.add(data)
+        kyokumen_list = copy.deepcopy(self.kyokumen_list)
+        return kyokumen_list, self.result
 
 
+def mjlog_to_round_data(mjlog_file):
+    mjdata_dict = parse_mjlog(load_mjlog(mjlog_file))
+    meta_data = mjdata_dict['meta']
+    player_rate = [float(player['rate']) for player in meta_data['UN']]
+    rule_set = meta_data['GO']['config']
+    data_list = []
+    for round in mjdata_dict['rounds']:
+        roundsim = RoundSimulator(player_rate=player_rate)
+        kyokumen_list, result = roundsim.read_round(round)
+        data_list.append({
+            'rule_set': rule_set,
+            'action_list': kyokumen_list,
+            'result': result
+        })
+    return data_list
